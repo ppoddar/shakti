38960f6789e70a3c87486902ac32246bdbda19bc
Pinaki Poddar <pinaki.poddar@gmail.com>
2019-11-05 17:45:52 -0800

    add audio files

--- scripts/poems.js
+++ scripts/poems.js
@@ -1,13 +1,24 @@
 var toc = [ 
-  { href:  "poem1.html", title: "Melodies of Joy", 
-	audio: "https://www.youtube.com/v/a5ZOKGw3IvE?&autoplay=1"},
-  { href:"poem2.html", title: "Key"},
-  { href:"poem3.html", title: "Oboni, Are you home?"},
-  {	href:"poem4.html", title: "Anarchist"},
-  { href:"poem5.html", title: "Not a Happy Hour"},
-  { href:"poem6.html", title: "Postmen of Forest of Fall"},
-  { href:"poem7.html", title: "Take me back", "audio":"audio/jarasandha.mp3"},
-  { href:"poem8.html", title: "May Go, But Why?"}
+  { 	href:  "poem1.html", 
+	  	title: "Melodies of Joy", 
+	  	audio: "audio/melodies_of_joy.mp3"},
+  { 	href:"poem2.html", 
+	  	title: "Key"},
+  { 	href:"poem3.html", 
+	  	title: "Oboni, Are you home?"},
+  {		href:"poem4.html", 
+	  	title: "Anarchist"},
+  { 	href:"poem5.html", 
+	  	title: "Not a Happy Hour"},
+  { 	href:"poem6.html", 
+	  	title: "Postmen of Forest of Fall",
+	  	audio: "audio/hemanter_aryna.mp3"},
+  { 	href:"poem7.html", 
+	  	title: "Take me back", 
+	  	audio: "audio/jarasandha.mp3"},
+  { 	href:"poem8.html", 
+	  	title: "May Go, But Why?",
+		audio:'audio/i_may_go.mp3'}
 ]
 
 $().ready(function() {
@@ -46,12 +57,33 @@ function createPoemEntry(poem) {
 }
 
 function showPoem(poem) {
-	$('#poem-title').text(poem.title)
-	$('#poem-title').css('font-weight', 'bold')
+	var $section = $('#poem-title-section')
+	$section.empty()
+	var $title = $('<p>')
+	$title.text(poem.title)
+	$title.css('font-weight', 'bold')
+	$section.append($title)
 	$('#english').load('./content/english/' + poem.href)
 	$('#bangla').load('./content/bangla/' + poem.href)
 	updateNavigation(poem)
+	
+	if (poem.audio) {
+		var $audio = create_audio_control(poem)
+		$section.append($audio)
+	} 
 }
+
+function create_audio_control(poem) {
+	var $audio = $('<audio controls>')
+	var $source = $('<source>')
+	$source.attr('src', poem.audio)
+	$source.attr('type', 'audio/mp3')
+	$audio.append($source)
+	$audio.append($source)
+	
+	return $audio
+}
+
 LEFT_ARROW_UNICODE  = '\u25C0'
 RIGHT_ARROW_UNICODE = '\u25B6'
 
@@ -63,7 +95,6 @@ function updateNavigation(poem) {
 function nextPoem(poem) {
 	var $next = $('#next-poem')
 	$next.empty()
-	$next.text(RIGHT_ARROW_UNICODE)
 	for (var i = 0; i < toc.length-1; i++) {
 		if (toc[i].href == poem.href) {
 			var next = toc[i+1]
@@ -78,7 +109,6 @@ function nextPoem(poem) {
 function prevPoem(poem) {
 	var $prev = $('#prev-poem')
 	$prev.empty()
-	$prev.text(LEFT_ARROW_UNICODE)
 	for (var i = 1; i < toc.length; i++) {
 		if (toc[i].href == poem.href) {
 			var prev = toc[i-1]
